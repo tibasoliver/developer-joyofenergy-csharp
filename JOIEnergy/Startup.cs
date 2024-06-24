7a940f670bbbeafe45267f1fbad3c95f7499a126
Thiago Oliveira <33914941+tibasoliver@users.noreply.github.com>
2024-06-23 22:22:21 -0300

    Add Swagger

--- JOIEnergy/Startup.cs
+++ JOIEnergy/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OpenApi.Models;
 
 namespace JOIEnergy
 {
@@ -28,6 +29,15 @@ namespace JOIEnergy
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "JOI Energy",
+                    Version = "v1"
+                });
+            });
+
             var readings =
                 GenerateMeterElectricityReadings();
 
@@ -67,6 +77,14 @@ namespace JOIEnergy
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+
+                app.UseSwagger();
+
+                app.UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "JOI Energy V1");
+                    c.RoutePrefix = "swagger";
+                });
             }
 
             app.UseRouting();
